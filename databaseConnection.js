ad39a76b4d259808644f066043e1d131688e0f0f
Addyware <adnanchahinbcit@gmail.com>
2025-02-17 22:06:27 -0800

    Updated database connection for FreeDB

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,27 +3,23 @@ const mysql = require('mysql2/promise');
 const is_hosted = process.env.IS_HOSTED || false;
 
 const dbConfigHosted = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
-	multipleStatements: false
+    host: "mysql-3a12664d-adnan-cf4d.h",  
+    port: 13261,                          
+    user: "avnadmin",                      
+    password: "Shootas456.",      
+    database: "FREEDB",  
+    multipleStatements: false
 };
 
 const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "database1",
-	multipleStatements: false
+    host: "localhost",
+    user: "root",
+    password: "Password",
+    database: "database1",
+    multipleStatements: false
 };
 
-if (is_hosted) {
-	var database = mysql.createPool(dbConfigHosted);
-}
-else {
-	var database = mysql.createPool(dbConfigLocal);
-}
+// Choose hosted database when deployed, otherwise use local database
+const database = is_hosted ? mysql.createPool(dbConfigHosted) : mysql.createPool(dbConfigLocal);
 
 module.exports = database;
-		
